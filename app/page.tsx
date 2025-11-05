44c9c8057a79b1257b92098a4267c3c5eb2072dd
Alessandro Argenziano <1603alessandro@gmail.com>
2025-11-05 23:09:34 +0100

    feat: installed all the components that i need from shadcn, created some custom variants for the button component

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex gap-4">
       {" "}
-      <Button variant="destructive" size="lg">
-        Click me
-      </Button>
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Link</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritary</Button>
     </div>
   );
 }
