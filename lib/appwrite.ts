15fe3406321be8f6753f8f8bd9dd19a9a8fbf22f
Alessandro Argenziano <1603alessandro@gmail.com>
2025-11-11 15:24:47 +0100

    feat: the database is working and we can now correctly create an user and log in

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,7 +2,7 @@ import "server-only";
 
 import { Client, Account, Storage, Users, Databases } from "node-appwrite";
 
-export async function createAdminClinet() {
+export async function createAdminClient() {
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
