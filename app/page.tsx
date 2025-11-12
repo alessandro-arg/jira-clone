75204c96d4eb6ba6c45e1dbb878b5122f26a684b
Alessandro Argenziano <1603alessandro@gmail.com>
2025-11-12 10:20:02 +0100

    feat: created a user button dropdown and added the logout fucntion

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useCurrent } from "./features/auth/api/use-current";
 import { useRouter } from "next/navigation";
 import { useLogout } from "./features/auth/api/use-logout";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "./features/auth/components/user-button";
 
 export default function Home() {
   const router = useRouter();
@@ -16,10 +17,10 @@ export default function Home() {
       router.push("/sign-in");
     }
   }, [data]);
+
   return (
     <div>
-      Only visible to authorized users.
-      <Button onClick={() => mutate()}> Logout</Button>
+      <UserButton />
     </div>
   );
 }
