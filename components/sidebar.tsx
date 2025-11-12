dfc60122f93d562d9a4ffd4f4c34a7eb3b946c52
Alessandro Argenziano <1603alessandro@gmail.com>
2025-11-12 11:40:39 +0100

    feat: created the (dashboard) route, now i'm working on the dashboard sidebar component

---
+++ components/sidebar.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+import DottedSeparator from "./dotted-separator";
+import { Navigation } from "./navigation";
+
+export const Sidebar = () => {
+  return (
+    <aside className="h-full bg-neutral-100 p-4 w-full">
+      <Link href="/">
+        <Image
+          src="/logo.svg"
+          alt="logo"
+          width={164}
+          height={48}
+          loading="eager"
+          className="w-41 h-12"
+        />
+      </Link>
+      <DottedSeparator className="my-4" />
+      <Navigation />
+    </aside>
+  );
+};
