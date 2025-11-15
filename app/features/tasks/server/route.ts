a896cd727a1eed67acc0c4f663d34a03d85a3871
Alessandro Argenziano <1603alessandro@gmail.com>
2025-11-15 19:55:23 +0100

    feat: add task listing with filters and populate project & assignee data

--- app/features/tasks/server/route.ts
+++ app/features/tasks/server/route.ts
@@ -3,62 +3,186 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createTaskSchema } from "../schemas";
 import { getMember } from "../../members/utils";
-import { DATABASE_ID, TASKS_ID } from "@/config";
+import { DATABASE_ID, MEMBERS_ID, PROJECTS_ID, TASKS_ID } from "@/config";
 import { ID, Query } from "node-appwrite";
+import { z } from "zod";
+import { TaskStatus } from "../types";
+import { createAdminClient } from "@/lib/appwrite";
+import { Project } from "../../projects/types";
 
-const app = new Hono().post(
-  "/",
-  sessionMiddleware,
-  zValidator("json", createTaskSchema),
-  async (c) => {
-    const user = c.get("user");
-    const tables = c.get("tables");
-    const { name, status, workspaceId, projectId, dueDate, assigneeId } =
-      c.req.valid("json");
+const app = new Hono()
+  .get(
+    "/",
+    sessionMiddleware,
+    zValidator(
+      "query",
+      z.object({
+        workspaceId: z.string(),
+        projectId: z.string().nullish(),
+        assigneeId: z.string().nullish(),
+        status: z.enum(TaskStatus).nullish(),
+        search: z.string().nullish(),
+        dueDate: z.string().nullish(),
+      })
+    ),
+    async (c) => {
+      const { users } = await createAdminClient();
+      const tables = c.get("tables");
+      const user = c.get("user");
 
-    const member = await getMember({
-      tables,
-      workspaceId,
-      userId: user.$id,
-    });
+      const { workspaceId, projectId, status, search, assigneeId, dueDate } =
+        c.req.valid("query");
 
-    if (!member) {
-      return c.json({ error: "Unauthorized" }, 403);
-    }
-
-    const highestPositionTask = await tables.listRows({
-      databaseId: DATABASE_ID,
-      tableId: TASKS_ID,
-      queries: [
-        Query.equal("status", status),
-        Query.equal("workspaceId", workspaceId),
-        Query.orderAsc("position"),
-        Query.limit(1),
-      ],
-    });
-
-    const newPosition =
-      highestPositionTask.rows.length > 0
-        ? highestPositionTask.rows[0].position + 1000
-        : 1000;
-
-    const task = await tables.createRow({
-      databaseId: DATABASE_ID,
-      tableId: TASKS_ID,
-      rowId: ID.unique(),
-      data: {
-        name,
-        status,
+      const member = await getMember({
+        tables,
         workspaceId,
-        projectId,
-        dueDate,
-        assigneeId,
-        position: newPosition,
-      },
-    });
+        userId: user.$id,
+      });
 
-    return c.json({ data: task });
-  }
-);
+      if (!member) {
+        return c.json({ error: "Unauthorized" }, 403);
+      }
+
+      const query = [
+        Query.equal("workspaceId", workspaceId),
+        Query.orderDesc("$createdAt"),
+      ];
+
+      if (projectId) {
+        console.log("projectId: ", projectId);
+        query.push(Query.equal("projectId", projectId));
+      }
+
+      if (status) {
+        console.log("status: ", status);
+        query.push(Query.equal("status", status));
+      }
+
+      if (assigneeId) {
+        console.log("assigneeId: ", assigneeId);
+        query.push(Query.equal("assigneeId", assigneeId));
+      }
+
+      if (dueDate) {
+        console.log("dueDate: ", dueDate);
+        query.push(Query.equal("dueDate", dueDate));
+      }
+
+      if (search) {
+        console.log("search: ", search);
+        query.push(Query.search("name", search));
+      }
+
+      const tasks = await tables.listRows({
+        databaseId: DATABASE_ID,
+        tableId: TASKS_ID,
+        queries: query,
+      });
+
+      const projectIds = tasks.rows.map((task) => task.projectId);
+      const assigneeIds = tasks.rows.map((task) => task.assigneeId);
+
+      const projects = await tables.listRows<Project>({
+        databaseId: DATABASE_ID,
+        tableId: PROJECTS_ID,
+        queries:
+          projectIds.length > 0 ? [Query.contains("$id", projectIds)] : [],
+      });
+
+      const members = await tables.listRows({
+        databaseId: DATABASE_ID,
+        tableId: MEMBERS_ID,
+        queries:
+          assigneeIds.length > 0 ? [Query.contains("$id", assigneeIds)] : [],
+      });
+
+      const assignees = await Promise.all(
+        members.rows.map(async (member) => {
+          const user = await users.get(member.userId);
+          return {
+            ...member,
+            name: user.name,
+            email: user.email,
+          };
+        })
+      );
+
+      const populatedTasks = tasks.rows.map((task) => {
+        const project = projects.rows.find(
+          (project) => project.$id === task.projectId
+        );
+        const assignee = assignees.find(
+          (assignee) => assignee.$id === task.assigneeId
+        );
+
+        return {
+          ...task,
+          project,
+          assignee,
+        };
+      });
+
+      return c.json({
+        data: {
+          ...tasks,
+          rows: populatedTasks,
+        },
+      });
+    }
+  )
+  .post(
+    "/",
+    sessionMiddleware,
+    zValidator("json", createTaskSchema),
+    async (c) => {
+      const user = c.get("user");
+      const tables = c.get("tables");
+      const { name, status, workspaceId, projectId, dueDate, assigneeId } =
+        c.req.valid("json");
+
+      const member = await getMember({
+        tables,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member) {
+        return c.json({ error: "Unauthorized" }, 403);
+      }
+
+      const highestPositionTask = await tables.listRows({
+        databaseId: DATABASE_ID,
+        tableId: TASKS_ID,
+        queries: [
+          Query.equal("status", status),
+          Query.equal("workspaceId", workspaceId),
+          Query.orderAsc("position"),
+          Query.limit(1),
+        ],
+      });
+
+      const newPosition =
+        highestPositionTask.rows.length > 0
+          ? highestPositionTask.rows[0].position + 1000
+          : 1000;
+
+      const task = await tables.createRow({
+        databaseId: DATABASE_ID,
+        tableId: TASKS_ID,
+        rowId: ID.unique(),
+        data: {
+          name,
+          status,
+          workspaceId,
+          projectId,
+          dueDate,
+          assigneeId,
+          position: newPosition,
+        },
+      });
+
+      return c.json({ data: task });
+    }
+  );
 
 export default app;
