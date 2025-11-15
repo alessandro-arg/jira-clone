5fb71a57eb41e7111467ec8c2ca206609fd7ff55
Alessandro Argenziano <1603alessandro@gmail.com>
2025-11-15 19:26:45 +0100

    feat: defined tasks schemas and types, created the post route for the tasks so that we can create some

--- app/features/tasks/components/task-view-switcher.tsx
+++ app/features/tasks/components/task-view-switcher.tsx
@@ -1,4 +1,7 @@
+import DottedSeparator from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { PlusIcon } from "lucide-react";
 
 export const TaskViewSwitcher = () => {
   return (
@@ -6,17 +9,44 @@ export const TaskViewSwitcher = () => {
       <div className="h-full flex flex-col overflow-auto p-4">
         <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
           <TabsList className="w-full lg:w-auto">
-            <TabsTrigger value="table" className="h-8 w-full lg:w-auto">
+            <TabsTrigger
+              value="table"
+              className="h-8 w-full lg:w-auto min-w-20"
+            >
               Table
             </TabsTrigger>
-            <TabsTrigger value="kanban" className="h-8 w-full lg:w-auto">
+            <TabsTrigger
+              value="kanban"
+              className="h-8 w-full lg:w-auto min-w-20"
+            >
               Kanban
             </TabsTrigger>
-            <TabsTrigger value="calendar" className="h-8 w-full lg:w-auto">
+            <TabsTrigger
+              value="calendar"
+              className="h-8 w-full lg:w-auto min-w-20"
+            >
               Calendar
             </TabsTrigger>
           </TabsList>
+          <Button size="sm" className="w-full lg:w-auto">
+            <PlusIcon className="size-4" />
+            New
+          </Button>
         </div>
+        <DottedSeparator className="my-4" />
+        Data filters
+        <DottedSeparator className="my-4" />
+        <>
+          <TabsContent value="table" className="mt-0">
+            Data table
+          </TabsContent>
+          <TabsContent value="kanban" className="mt-0">
+            Data kanban
+          </TabsContent>
+          <TabsContent value="calendar" className="mt-0">
+            Data calendar
+          </TabsContent>
+        </>
       </div>
     </Tabs>
   );
