9c8aecf9b8a67ba629e7d9ed2df673463c8dc8c9
Alessandro Argenziano <1603alessandro@gmail.com>
2025-11-17 20:51:33 +0100

    feat: implemented the use-task-filters hook to use correctly the status of the task in the url

--- app/features/tasks/components/data-filters.tsx
+++ app/features/tasks/components/data-filters.tsx
@@ -11,6 +11,7 @@ import { useGetProjects } from "../../projects/api/use-get-projects";
 import { useWorkspaceId } from "../../workspaces/hooks/use-workspace-id";
 import { ListCheckIcon } from "lucide-react";
 import { TaskStatus } from "../types";
+import { useTaskFilters } from "../hooks/use-task-filters";
 
 interface DataFiltersProps {
   hideProjectFilter?: boolean;
@@ -38,11 +39,27 @@ export const DataFilters = ({ hideProjectFilter }: DataFiltersProps) => {
     label: member.name,
   }));
 
+  const [{ status, assigneeId, projectId, dueDate }, setFilters] =
+    useTaskFilters();
+
+  const onStatusChange = (value: string) => {
+    if (value === "all") {
+      setFilters({ status: null });
+    } else {
+      setFilters({ status: value as TaskStatus });
+    }
+  };
+
   if (isLoading) return null;
 
   return (
     <div className="flex flex-col lg:flex-row gap-2">
-      <Select defaultValue={undefined} onValueChange={() => {}}>
+      <Select
+        defaultValue={status || undefined}
+        onValueChange={(value) => {
+          onStatusChange(value);
+        }}
+      >
         <SelectTrigger className="w-full lg:w-auto h-8">
           <div className="flex items-center pr-2">
             <ListCheckIcon className="size-4 mr-2" />
